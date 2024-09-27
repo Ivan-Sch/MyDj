936b106aadf64337148cd00bb00485b8368d566d
Ivan <serbakovv79@gmail.com>
2024-09-27 12:28:37 +0400

    Создание представлений в prog
    1) Создание urls.py в приложении prog для изолирования приложения
    2) Создание прдеставлений в prog views.py: две функции index and categories
    3) В prog urls.py пропись обращений к представлениям (к функциям) views.py через список urlpatterns
    4) В базе проекта siteprog в urls пропись обращения к приложению prog, а именно к prog urls через include

--- siteprog/prog/views.py
+++ siteprog/prog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def index(request):  # Class HttpRequest
+    return HttpResponse("Вы на главной странице")
+
+
+def categories(request):
+    return HttpResponse("<h1>Темы по категориям</h1>")
